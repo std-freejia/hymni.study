22e0b6fe7e26e906669d33c6c9600ff0ad31b35e
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2020-06-25 21:16:34 +0900

    sun

---
+++ Code/20200624_printer_sun.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 1;
+    int first_id = -1;
+    int check_id = 0;
+    int len = priorities.size();
+    
+    vector<int> ascending_order;
+    
+    ascending_order.assign(priorities.begin(), priorities.end());
+    
+    sort(ascending_order.begin(), ascending_order.end());
+    
+    while(1)
+    {
+        check_id = (first_id + 1) % len;
+        
+        while(1)
+        {
+            if(ascending_order.back() == priorities[check_id])
+            {
+                ascending_order.pop_back();
+                
+                priorities[check_id] = 0;
+                
+                first_id = check_id;
+                
+                if(first_id == location)
+                {
+                    return answer;
+                }
+                else
+                {
+                    answer++;
+                    break;
+                }
+            }
+            else
+            {
+                check_id = (check_id + 1) % len;
+            }
+        }
+    }
+}
