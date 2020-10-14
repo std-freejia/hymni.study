714333a7a2ab2f2fbd5d1b6efb86eb06b7d5f0a8
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2020-10-14 15:49:44 +0900

    sun

--- Code/20201011_GCD_and_LCM_sun.cpp
+++ Code/20201011_GCD_and_LCM_sun.cpp
@@ -1,3 +1,8 @@
+/* 
+1. 유클리드 호제법으로 최대공약수 계산
+2. 최소공배수 = 곱 / 최대공약수
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -9,7 +14,6 @@ int main(int argc, char* argv[], char* envs[])
 {
     int num1 = 0;
     int num2 = 0;
-    int gcd = 0;
     
     cin>>num1>>num2;
     
@@ -22,6 +26,8 @@ int main(int argc, char* argv[], char* envs[])
 
 int GCD(int num1, int num2)
 {
+    // 유클리드 호제법
+    
     if(num1 % num2 == 0)
     {
         return num2;
@@ -34,5 +40,7 @@ int GCD(int num1, int num2)
 
 int LCM(int num1, int num2)
 {
+    // 최소공배수 = 곱 / 최대공약수
+    
     return ((num1 * num2) / GCD(num1, num2));
 }
