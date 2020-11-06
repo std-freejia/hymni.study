14f69c0d16bfc7aa7596539ca6d98001b6fdd7ce
Lee Dong Hyuk <36650428+wizdom5@users.noreply.github.com>
2020-11-06 18:16:08 +0900

    Update 20201102_Suffix_Array_LDH.c

--- Code/20201102_Suffix_Array_LDH.c
+++ Code/20201102_Suffix_Array_LDH.c
@@ -9,28 +9,32 @@ int compare(const void *a, const void *b);
 int main(int argc, char* argv[], char* env[])
 {
 	char string[1000];
-	char temp[1000];
 	
 	scanf("%s", string);
 	
 	int size = strlen(string);
-	
+	char temp[size][1000];
+
+	//입력값 앞자리 하나씩 잘라주면서 temp배열에 저장
 	for(int i = 0; i < size; i++)
 	{
-		strncpy(&temp[i], string+i, size-i);
+		strncpy(temp[i], string+i, size-i);
 	}
-	
+		
+	//퀵정렬 이용해서 문자열 정렬
 	qsort(temp, sizeof(temp)/sizeof(temp[0]), sizeof(temp[0]), compare);
 	
+	//정렬된 temp배열 출력
 	for(int i = 0; i < size; i++)
 	{
-		printf("%s\n", &temp[i]);
+		printf("%s\n", temp[i]);
 	}
 	
 	return 0;
 }
 
-int compare(const void *a, const void *b)    // 오름차순 비교 함수 구현
+// 오름차순 비교 함수 구현
+int compare(const void *a, const void *b)
 {
     return strcmp((char *)a, (char *)b);
 }
