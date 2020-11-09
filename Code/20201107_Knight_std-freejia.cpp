37a3d2298ddd54689eeae92c9c432455febd08e3
DESKTOP-8F4SV0M\stdfreejia <std.freejia@gmail.com>
2020-11-10 00:13:17 +0900

    나이트의 이동 (BFS) - 지아

--- Code/20201107_Knight_std-freejia.cpp
+++ Code/20201107_Knight_std-freejia.cpp
@@ -1,84 +1,81 @@
 #include <iostream> 
 #include <queue>
 #include <cstring> // memset()
-#include <algorithm> // min(), pair STL
-#define M_MAX 300
+#include <algorithm>
+ 
 using namespace std;
 
+// 나이트의 이동(BOJ 7562)  
 
-int M[M_MAX+1][M_MAX+1]; // 체스판  
-int M_LIMIT; // 체스판 크기 
+int TC, N, answer; // 테스트케이스, 체스판 한 변의 길이
+int map[301][301]; 
+int start_x, start_y, dest_x, dest_y; // 시작지점, 목적지점  
 
-int a,b, x,y; // 현재좌표, 목적좌표  
+int xx[9] = {-2, -1, 1, 2, -2, -1, 1, 2};
+int yy[9] = {-1, -2, 2, 1, 1, 2, -2, -1};
 
-// 8방향 
-int dx[10] = {1, 2, 2, 1, -2, -1, -2, -1}; 
-int dy[10] = {2, 1, -1, -2, 1, 2, -1, -2};
+bool boundary(int x, int y){
+	return (x >= 0 && x < N && y >= 0 && y < N)? 1:0;
+}
 
-
-// 범위 내에 있는지 확인
-bool boundary(int i, int j){   
-
-	return (i >= 0 && i < M_LIMIT && j >= 0 && j < M_LIMIT) ? 1:0;
-} 
-
-
-int BFS(){
-
-	int cnt = 0;
+void BFS(){
 	
-	// q : 현재 좌표{a,b}와 이동 횟수cnt 저장  
-	queue<pair<pair<int,int>,int>> q;	
-
-	q.push({{a, b}, cnt} ); // 현재좌표 푸시  
-	M[a][b] = 1; // 현재좌표 방문표시
-	 
+	queue<pair<pair<int,int>, int> > q;
+	
+	q.push({{start_x, start_y}, 0}); // 시작지점 큐에 넣기  
+	map[start_x][start_y] = 1; // 방문표시 
+	
 	while(!q.empty()){
 	
-		pair<pair<int,int>,int> now = q.front();
+		int now_x = q.front().first.first;
+		int now_y = q.front().first.second;
+		int now_cnt = q.front().second;
+		
 		q.pop();
 		
-		int nx = now.first.first; // 현재 좌표  
-		int ny = now.first.second; 
-		int cnt = now.second; // cnt 
+		if(now_x == dest_x && now_y == dest_y){ // 목적지 도착하면 탈출  
+			answer = now_cnt; 
+			break;
+		} 
 		
-		// 목적지라면 종료 
-		if(nx == x && ny == y){
-			return cnt;
-		} 
+		for(int i = 0; i < 8; i++){ // 8방향 모두 확인  
+			int next_x = now_x + xx[i];
+			int next_y = now_y + yy[i];
+			
+			if(false == boundary(next_x, next_y)) continue; // 인덱스 확인 
+			if(1 == map[next_x][next_y]) continue; // 방문했었는지 확인  
+			
+			q.push({{next_x, next_y}, now_cnt+1}); // 큐에 푸시  
+			map[next_x][next_y] = 1; // 방문표시  
+		}
+	} 
+	
+	
+}
 
-		// 8 방향 확인  
-		for(int i = 0; i < 8; i++){ // 범위 내부인지 확인 , 안가봤는지 확인, 가보면서 cnt++
-			
-			int tmpX = nx + dx[i]; // 가볼 좌표  
-			int tmpY = ny + dy[i];
-			
-			if(boundary(tmpX, tmpY)){ // 범위 내부이면 
-				
-				if(M[tmpX][tmpY] == 0){ // 안가봤다면  
-					
-					q.push({ {tmpX, tmpY}, cnt+1} ); // 큐에 푸시
-					M[tmpX][tmpY] = 1; //방문표시   
-				}
-			}
-		} 
+void input(){
+
+	cin >> TC; 
+	
+	while(TC--){
+		
+		cin >> N; // 체스판 길이  
+		
+		scanf("%d %d %d %d", &start_x, &start_y, &dest_x, &dest_y);
+		
+		BFS(); // 탐색  
+		
+		cout << answer << '\n';
+		
+		memset(map, 0, sizeof(map)); // 방문 배열 초기화  
+		
+		answer = 0;
 	}
 }
 
-int main(void){
- 
- 	int TC = 0;
- 	
-	cin >> TC;
-	
-	while(TC--){
-	
-		cin >> M_LIMIT; 
-		cin >> a >> b >> x >> y; // 현재좌표, 목적좌표 
-		memset(M, 0, sizeof(M));
- 		cout << BFS() << '\n';	
-		
- 	}
-	
-	return 0;	
-}
+int main() {	
+
+ 	input();
+
+	return 0;
+} 
