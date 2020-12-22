24363000ae69abe81087356a805dafb240db2649
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2020-12-22 14:04:31 +0900

    sun

---
+++ Code/20201219_Correct_parenthesis_sun.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+    
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if(st.empty())
+            {
+                answer = false;
+                
+                break;
+            }
+            
+            st.pop();
+        }
+    }
+    
+    if(!st.empty())
+    {
+        answer = false;
+    }
+    
+    return answer;
+}
