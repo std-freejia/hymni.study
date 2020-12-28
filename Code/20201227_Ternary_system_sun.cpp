a79f1d9592adccdc606db69245899372cbcdaf99
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2020-12-28 09:12:38 +0900

    sun

---
+++ Code/20201227_Ternary_system_sun.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+const int DIVISOR = 3;  // 3진법
+
+int solution(int n) {
+    int answer = 0;
+    string s = "";  // 앞뒤 반전 3진법 저장
+    
+    // 앞뒤 반전 3진법 계산
+    while(n)
+    {
+        s += (n % DIVISOR);
+        
+        n /= DIVISOR;
+    }
+    
+    // 10진법으로 표현
+    for(int i=0; i<s.size(); i++)
+    {
+        answer += (s[i] * pow(DIVISOR, s.size() - i - 1));
+    }
+    
+    return answer;
+}
