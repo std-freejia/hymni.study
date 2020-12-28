1d08653613f0bd54bac23eb6442ac5c98b9d3d68
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2020-12-28 09:21:32 +0900

    sun

---
+++ Code/20201227_Add_numbers_sun.cpp
@@ -0,0 +1,31 @@
+/*
+1. In a set, the value of each element must be unique.
+2. The elements in a set are always sorted.
+*/
+
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    set<int> sum;
+    int i = 0;  // for loop
+    int j = 0;  // for loop
+    int size = numbers.size();
+
+    // 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 set에 저장
+    for(i=0; i<size; i++)
+    {
+        for(j=i+1; j<size; j++)
+        {
+            sum.insert(numbers[i] + numbers[j]);
+        }
+    }
+
+    answer.assign(sum.begin(), sum.end());
+    
+    return answer;
+}
