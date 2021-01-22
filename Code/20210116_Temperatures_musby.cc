8adf28a5eb93314d6b49b518abe153c01702f141
achieve365 <59115771+achieve365@users.noreply.github.com>
2021-01-22 13:44:13 +0900

    Codingame GOGO

---
+++ Code/20210116_Temperatures_musby.cc
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+int main()
+{
+    // the number of temperatures to analyse
+    int n, min = 0;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        
+        int t;
+        scanf("%d", &t);
+        
+        (0 == i) ? min = INT_MAX : false;
+
+        if(abs(t) < abs(min)){
+            min = t;
+        }else if(abs(t) == abs(min)){
+            min = abs(t);
+        }
+    }
+
+    printf("%d\n", min);
+
+    return 0;
+}
