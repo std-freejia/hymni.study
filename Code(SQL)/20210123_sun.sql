a9b75344e0658c70668daaab3a5d6480bb917763
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2021-01-26 13:39:58 +0900

    sun

---
+++ Code(SQL)/20210123_sun.sql
@@ -0,0 +1,27 @@
+// Employee Salaries
+SELECT name
+FROM Employee
+WHERE salary > 2000 AND months < 10;
+
+// Employee Names
+SELECT name
+FROM Employee
+ORDER BY name
+
+// African Cities
+SELECT CITY.NAME
+FROM CITY, COUNTRY
+WHERE CITY.CountryCode = COUNTRY.Code AND COUNTRY.CONTINENT = 'Africa';
+
+// Higher Than 75 Marks
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID ASC
+
+// Top Earners
+SELECT salary * months AS earnings, COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
