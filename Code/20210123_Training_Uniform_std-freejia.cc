96292cf71eaa07cbd14d988852dbf7b8b7d67901
DESKTOP-8F4SV0M\stdfreejia <std.freejia@gmail.com>
2021-01-30 23:58:11 +0900

    지아

--- Code/20210123_Training_Uniform_std-freejia.cc
+++ Code/20210123_Training_Uniform_std-freejia.cc
@@ -1,8 +1,8 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
+// 체육복 
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     
@@ -10,26 +10,25 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 
     vector<int> student(n+1, 1); // 1로 초기화 
 
-    for (int i = 0;  i < reserve.size(); i++) { // 여벌 있으면 + 1 
-        student[reserve[i]]++;
+    
+    for (auto i : reserve) { // 여벌 있으면 + 1 
+        student[i]++;
     }
 
-    for (int i = 0; i < lost.size(); i++) { // 도난 당했다면 -1
-        student[lost[i]]--;
-        if (student[lost[i]] == 0) answer--;
+    for (auto i : lost) { // 도난 당했다면 -1
+        student[i]--;
+        if (student[i] == 0) answer--;
     }
 
-
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) { // 빌려줄 수 있는 경우는 (2, 0) 그리고 (0, 2) 인 경우 뿐이다. 
 
         if (student[i] == 2 && student[i+1] == 0) {
             student[i] = student[i + 1] = 1;
             answer++;
-            continue;
+            
         }else if(student[i] == 0 && student[i + 1] == 2) {
             student[i] = student[i + 1] = 1;
             answer++;
-            continue;
         }
     }
 
