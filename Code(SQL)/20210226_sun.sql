1e46e50467cb84525bb864ba74e0f87ffed8f4b0
Sundislikesun <44043438+Sundislikesun@users.noreply.github.com>
2021-02-25 16:17:39 +0900

    sun

---
+++ Code(SQL)/20210226_sun.sql
@@ -0,0 +1,28 @@
+/* Revising the Select Query I */
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+/* Revising the Select Query II */
+SELECT NAME
+FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+/* Select All */
+SELECT *
+FROM CITY;
+
+/* Select By ID */
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+
+/* Japanese Cities' Attributes */
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+/* Japanese Cities' Names */
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
